67d6faead3eb811c4b5837a84256ff01c25af38a
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:13:59 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Do while_Loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+     int i=1; //initialization                                 //for initialization ; condition ; counterUpdate
+
+     do
+   {
+
+     printf("Sadiqul Islam Shakib\n");
+
+       printf("%d\n",i);
+
+       i++; //increment
+                                                 //Loop = for , while , do while
+   }while(i<=10); //condition
+
+     return 0;
+}
+

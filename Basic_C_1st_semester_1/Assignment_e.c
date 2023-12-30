7dbdb5b66b07688a918b5b551dac7c0e1c0bb9b1
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 21:59:56 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Assignment_e.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int i=0,x=0;
+    for(i=1; i<10; ++i)
+    {
+        if(i%2==1)
+            x+=1;
+        else
+            x--;
+        printf("%d",x);
+        continue;
+    }
+
+    printf("\nx=%d",x);
+}

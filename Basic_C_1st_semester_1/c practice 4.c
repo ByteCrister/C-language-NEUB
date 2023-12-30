7ed588109ff57e3b09eec440a1818cb91e435b98
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:04:08 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/c practice 4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter a letter~~");
+    scanf("%c",&a);
+    switch(a)
+    {
+    case 'a':
+    case 'A':
+
+    case 'e':
+    case 'E':
+
+    case 'i':
+    case 'I':
+
+    case 'o':
+    case 'O':
+
+    case 'u':
+    case 'U':
+        printf("vowel");
+
+        break;
+
+    }
+    printf("consonant!");
+    return 0;
+}

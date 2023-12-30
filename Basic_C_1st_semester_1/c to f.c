ecf1ad82231fbf7b4d2e996b9e0c1184b332a839
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:04:38 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/c to f.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+       float c,f;
+
+       printf("enter tempereture=");
+       scanf("%f",&c);
+       f=((c*9/5)+32);
+       printf("\n the result is=%2f",f);
+       getch();
+
+
+
+
+
+
+}

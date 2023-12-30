6666e404334b75558e7b1ffe9c00c496c9ecf862
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:08:12 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Calss program_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int x,n,i;
+
+    for(i=0; i<=100; i++)
+
+    {
+        scanf("%d",&n);
+        if(n%2==1)
+
+
+            printf("\n odd number :%d",n);
+        break;
+
+    }
+    printf("i= %d\n",i);
+
+    return 0;
+}
+

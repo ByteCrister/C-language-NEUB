daa6dd8ffd389f8c6783e73b5571c2177d46625b
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:15:09 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Even or odd program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int value;
+    printf("Enter a number=");
+    scanf("%d",&value);
+    if
+        (value%2==0)
+       {
+
+
+        printf("The number is even \n");
+        printf("Sadiqul islam shakib1 ");
+
+       }
+    else
+        printf("The number is odd");
+    getch();
+}

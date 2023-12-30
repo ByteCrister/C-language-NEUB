20b887ad6629f416d3c0ede23c98f7f8b7613bbc
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 21:57:42 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Assignment operator Program 2.0.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20,c=30,d=40,e=50;
+    a+=5;
+    printf(" The value of a+=%d\n");
+    b-=5;
+    printf(" The value of b-=%d\n");
+    c/=5;
+    printf(" The value of c/=%d\n");
+    d*=5;
+    printf(" The value of d*=%d\n");
+    e%=5;
+    printf(" The value of e%=%d\n");
+    return ;
+
+
+
+
+
+}

5b0aaba57822f79ce10a93536e1f20a440e3bce7
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 21:56:39 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Area of rectangle.c
@@ -0,0 +1,14 @@
+//a program that shows the area of rectangle
+#include<stdio.h>
+int main()
+{
+    float a,b,area;
+    printf("Enter lenght =");
+    scanf("%f",&a);
+    printf("Enter height =");
+    scanf("%f",&b);
+    area=a*b;
+    printf("The result is=%.5f",area);
+    getch();
+
+}

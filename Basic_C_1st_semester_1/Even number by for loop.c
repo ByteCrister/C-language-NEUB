903f72e4d163cab5612e44043e4649f8b157663c
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 22:14:45 +0600

    Add files via upload

---
+++ Basic_C_1st_semester_1/Even number by for loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+     int i,o=1;                                  //for initialization ; condition ; counterUpdate
+
+     for(i=5; i>0; i++)
+
+   {
+
+     printf("Sadiqul Islam Shakib\n");
+
+       printf("%d\n",i);                                           //Loop = for , while , do while
+   }
+
+     return 0;
+}
+

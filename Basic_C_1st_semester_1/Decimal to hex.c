18ae6c5a9336b56c2402f78df809a0fd6148950e
Sadiqul Islam Shakib <143107015+ByteCrister@users.noreply.github.com>
2023-12-30 21:52:09 +0600

    Delete Basic_C_1st_semester_1 directory

--- Basic_C_1st_semester_1/Decimal to hex.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int hex;
-    printf("Enter decimal number=");
-    scanf("%d",&hex);
-    printf("The hexadecimal number is= %x",hex);
-    getch();
-}
--- Basic_C_1st_semester_1/celling program.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-    double a=5.3;
-    double result=ceil(a);
-    printf("ceil(%.2lf)=%.2lf",a,result);
-    getch();
-}
